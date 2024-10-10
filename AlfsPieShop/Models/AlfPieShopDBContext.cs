f3d2355f98ca68274a7a80284d76c7b893751869
Eric Cronje <eric.cronje@gmail.com>
2024-10-11 04:35:32 +0800

    Update comments

--- AlfsPieShop/Models/AlfPieShopDBContext.cs
+++ AlfsPieShop/Models/AlfPieShopDBContext.cs
@@ -4,7 +4,7 @@ namespace AlfPieShop.Models
 {
     public class AlfPieShopDBContext(DbContextOptions options) : DbContext(options)
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; } //#50
+        public DbSet<Pie> Pies { get; set; } //#50
     }
 }
